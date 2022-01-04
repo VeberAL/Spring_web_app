9269e1cc6ac7d4996e9a46b0dc64dca908a17355
VeberAL <aleo83@rambler.ru>
2022-01-04 15:35:40 +0300

    CRUD операции на стороне DAO

--- src/main/java/ru/back/dao/PersonDAO.java
+++ src/main/java/ru/back/dao/PersonDAO.java
@@ -10,28 +10,35 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-
+    //Использование JDBCTemplate с защитой от sql-инъекций
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
+    //Поиск всех пользователей
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
-
+    //Поиск пользователя по указанному id
     public Person show(int id) {
-        return null;
+        //в методе query в знак ? передается значение указанное в скобках дальше
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
-
+    //Добавление нового пользователя
     public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail());
     }
-
+    //Обновление информации о пользователе
     public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
-
+    //Удаление пользователя
     public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
